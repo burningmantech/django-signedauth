feef16b994488b1197d378614db0a80ea2902cd6
Liam Kirsher <liamk@numenet.com>
2015-07-02 16:49:30 -0700

    Try find_packages

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
+from setuptools import find_packages
 
 setup(name='Signedauth',
       version='1.1',
@@ -8,7 +9,8 @@ setup(name='Signedauth',
       author='Bruce Kroeze',
       author_email='bruce@ecomsmith.com',
       url='https://github.com/LiamK/django-signedauth',
-      packages=['signedauth', 'signedauth.explore'],
+      #packages=['signedauth', 'signedauth.explore'],
+      packages=find_packages(),
       install_requires=[
         'django-keyedcache >= 1.4',
         'httplib2 >= 0.7.2',
